0b32e3eeade0431c81dfb978457d7d3b6fd137f6
Raomark <aekungkun.t@gmail.com>
2023-01-20 11:12:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: '2500',
+    reset: true
+})
+
+sr.reveal('.featured,.postup1,.postup2,.postup3',{origin:'bottom', delay: 200})
+sr.reveal('.side1,.side2,.side3',{origin:'right', delay: 200})
